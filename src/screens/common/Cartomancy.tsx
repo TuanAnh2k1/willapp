d981130f12bef384b3f72169e000922d800012ea
tuananh <tuananh2k1ptit@gmail.com>
2024-01-29 16:50:49 +0700

    core-cartomanycy

--- src/screens/common/Cartomancy.tsx
+++ src/screens/common/Cartomancy.tsx
@@ -1,17 +1,34 @@
 import { welcome } from "@assets";
-import { Toolbar } from "@components";
-import { theme } from "@utils";
-import { ImageBackground, SafeAreaView, StyleSheet, View } from "react-native";
+import { BaseButton, Toolbar } from "@components";
+import { colors, theme } from "@utils";
+import { useState } from "react";
+import { ImageBackground, SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
 
 const Cartomancy = () => {
+
+    const [card, setCard] = useState<number>();
+
+    const getZodiacMeaning = () => {
+        switch (card) {
+            case 1:
+                return "";
+            default:
+                return "- Không rõ ý nghĩa";
+        }
+    }
+
     return (
         <SafeAreaView style={theme.container}>
             <Toolbar
-                title={"Bói bài tây hàng ngày"}
+                title={"Bói bài hàng ngày"}
             />
-            <View style={styles.container}>
-
-            </View>
+            <ScrollView style={styles.container}>
+                <View style={styles.buttonPlease}>
+                    <Text style={styles.textPlease}>Xin thành tâm</Text>
+                </View>
+                <Text style={styles.textMeaning}>Ý nghĩa lá bài về sức khỏe, công việc, tình yêu trong 24 giờ tới: </Text>
+                <Text style={styles.textMeaningData}>{getZodiacMeaning()}</Text>
+            </ScrollView>
         </SafeAreaView>
     )
 }
@@ -19,7 +36,31 @@ const Cartomancy = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        marginHorizontal: 12,
     },
+    buttonPlease: {
+        alignItems: 'center',
+        marginVertical: 12,
+        width: '50%',
+    },
+    textPlease: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        backgroundColor: colors._EAA300,
+        color: colors.white,
+        textAlign: 'center',
+        borderRadius: 12,
+        paddingVertical: 8,
+    },
+    textMeaning: {
+        fontSize: 16,
+        paddingBottom: 12,
+        fontWeight: 'bold',
+        paddingTop: 6
+    },
+    textMeaningData: {
+        fontSize: 16,
+    }
 });
 
 export { Cartomancy };
